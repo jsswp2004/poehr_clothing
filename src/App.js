2699c48b3afc57a7ac92347b42d511b22e51b28d
jsswp2004 <31414054+jsswp2004@users.noreply.github.com>
2021-12-30 22:46:29 -0500

    zz

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import React from 'react';
+import { Routes,Route } from 'react-router-dom';
+
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+)
+  
+
 function App() {
   return (
     <div>
-      <HomePage />
+      <Routes>
+        <Route path='/' element={<HomePage />}/>
+        <Route path='/hats' element={<HatsPage/>}/>
+      </Routes>
     </div>
   );
 }
 
 export default App;
+//<Route path="/hats" component={HatsPage}/>
+//<Route exact path="/" component={HomePage} />
+//<HomePage />
