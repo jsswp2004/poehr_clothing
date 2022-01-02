8c2ff004fa12eedd2b9b069425a7259fa9f08afd
jsswp2004 <31414054+jsswp2004@users.noreply.github.com>
2022-01-01 19:04:54 -0500

    xx

--- src/App.js
+++ src/App.js
@@ -16,8 +16,8 @@ function App() {
   return (
     <div>
       <Routes>
-        <Route path='/' element={<HomePage />}/>
-        <Route path='/hats' element={<HatsPage/>}/>
+        <Route path='/' element={HomePage}/>
+        <Route path='/hats' element={HatsPage}/>
       </Routes>
     </div>
   );
