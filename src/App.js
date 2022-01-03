e453fbb358eab34c4d5f91a9f6f3e969369da6b0
jsswp2004 <31414054+jsswp2004@users.noreply.github.com>
2022-01-03 16:43:58 -0500

    done with lecture 82

--- src/App.js
+++ src/App.js
@@ -5,19 +5,14 @@ import { Routes, Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component.jsx';
 
-const HatsPage = () => (
-  <div className="hats">
-    <h1>HATS PAGE</h1>
-  </div>  
-);
- 
 function App() {
   return (
    <div className="App">
     <Routes>
       <Route path='/*' element={<HomePage />}/>
-      <Route path='/hats' element={<HatsPage />}/>
+      <Route path='/shop' element={<ShopPage />}/>
     </Routes>
    </div>
   );
