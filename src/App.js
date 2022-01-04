8a49583a42614c28bd7b44df50582773b93e1da2
jsswp2004 <31414054+jsswp2004@users.noreply.github.com>
2022-01-04 01:05:29 -0500

    ss

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
+import Header from './components/header/header.component.jsx';
 
 function App() {
   return (
-   <div className="App">
+    <div className="App">
+      <Header />
     <Routes>
       <Route path='/*' element={<HomePage />}/>
       <Route path='/shop' element={<ShopPage />}/>
