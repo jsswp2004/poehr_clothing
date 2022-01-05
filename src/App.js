e15d13101153988ab313dff1b3230bba6e960413
jsswp2004 <31414054+jsswp2004@users.noreply.github.com>
2022-01-04 20:32:46 -0500

    dd

--- src/App.js
+++ src/App.js
@@ -5,8 +5,9 @@ import { Routes, Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component.jsx';
-import Header from './components/header/header.component.jsx';
+import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 function App() {
   return (
@@ -14,7 +15,8 @@ function App() {
       <Header />
     <Routes>
       <Route path='/*' element={<HomePage />}/>
-      <Route path='/shop' element={<ShopPage />}/>
+        <Route path='/shop' element={<ShopPage />} />
+        <Route path='/signin' element={<SignInAndSignUpPage />}/>
     </Routes>
    </div>
   );
