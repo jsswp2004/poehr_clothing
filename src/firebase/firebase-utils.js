abb0941f0fe321f5069afa6770d7fd02bd261459
jsswp2004 <31414054+jsswp2004@users.noreply.github.com>
2022-01-05 07:31:25 -0500

    dd

---
+++ src/firebase/firebase-utils.js
@@ -0,0 +1,20 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const config = {
+  apiKey: "AIzaSyBfA_G_fy9QodknkL0IdsY--oURIFCkJPk",
+  authDomain: "poehr-db.firebaseapp.com",
+  projectId: "poehr-db",
+  storageBucket: "poehr-db.appspot.com",
+  messagingSenderId: "305539355313",
+  appId: "1:305539355313:web:ada80012ed8d679eb04890",
+  measurementId: "G-LTYBYFRLDL",
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
