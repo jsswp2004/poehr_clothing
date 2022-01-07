d781015f31ed6374cc0fe0f6f4b389dd72514e03
jsswp2004 <31414054+jsswp2004@users.noreply.github.com>
2022-01-06 20:52:49 -0500

    97

--- src/firebase/firebase-utils.js
+++ src/firebase/firebase-utils.js
@@ -1,6 +1,6 @@
-import firebase from "firebase/app";
-import "firebase/firestore";
-import "firebase/auth";
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+import "firebase/compat/auth";
 
 const config = {
   apiKey: "AIzaSyBfA_G_fy9QodknkL0IdsY--oURIFCkJPk",
@@ -19,6 +19,6 @@ export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({ prompt: 'select_account' });
-export const signInWithGoogle = () => auth.signInwithPopUp(provider);
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
