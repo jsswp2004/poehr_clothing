77443478f16649475e793ef10ace48fb1f594218
jsswp2004 <31414054+jsswp2004@users.noreply.github.com>
2022-01-10 17:22:15 -0500

    cc

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 
-import './index.css';
-import App from './App';
+import "./index.css";
+import App from "./App";
 
-
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  rootElement,
+  <Provider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  rootElement
 );
-
---
+++ src/redux/user/user.reducer.jsx
@@ -0,0 +1,15 @@
+const INITITAL_STATE = {
+    currentUser: null
+}
+
+
+const userReducer = (state = INITITAL_STATE, action) =>{
+switch (action.type) {
+    case '':
+        
+        break;
+
+    default:
+        break;
+}
+}
