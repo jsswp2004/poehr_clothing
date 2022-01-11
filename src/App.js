5f4665935eefcacd9d678bed1d453d4ef805ddda
jsswp2004 <31414054+jsswp2004@users.noreply.github.com>
2022-01-11 15:29:46 -0500

    error ingout

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
-import  { connect } from 'react-redux';
+import { Routes, Route, Navigate } from "react-router-dom";
+import { connect } from "react-redux";
+
 import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
-import Header from "./components/header/header.component";
+
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+import Header from "./components/header/header.component";
 import { auth, createUserProfileDocument } from "./firebase/firebase-utils";
-import { setCurrentUser } from './redux/user/user.actions';
+import { setCurrentUser } from "./redux/user/user.actions";
 
 class App extends React.Component {
-
   unsubscribeFromAuth = null;
 
   componentDidMount() {
@@ -22,11 +23,10 @@ class App extends React.Component {
 
         userRef.onSnapshot((snapShot) => {
           setCurrentUser({
-              id: snapShot.id,
-              ...snapShot.data()  
+            id: snapShot.id,
+            ...snapShot.data(),
           });
         });
-        
       }
 
       setCurrentUser({ userAuth });
@@ -44,15 +44,33 @@ class App extends React.Component {
         <Routes>
           <Route path="/*" element={<HomePage />} />
           <Route path="/shop" element={<ShopPage />} />
-          <Route path="/signin" element={<SignInAndSignUpPage />} />
+          <Route
+            path="/signin"
+            element={
+              this.props.currentUser ? (
+                <Navigate to="/" />
+              ) : (
+                <SignInAndSignUpPage />
+              )
+            }
+          />
         </Routes>
       </div>
     );
   }
 }
 
-const mapDispatchToProps = dispatch => ({
-  setCurrentUser: user=> dispatch(setCurrentUser(user))
-})
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser,
+});
 
-export default connect (null,mapDispatchToProps)(App);
+const mapDispatchToProps = (dispatch) => ({
+  setCurrentUser: (user) => dispatch(setCurrentUser(user)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
+//this.props.currentUser ? (
+//  <Navigate to="/" />
+//) : (
+//  <SignInAndSignUpPage />
+//)
