d2715951ba51e014f1f851f4aeda928f60646ad9
jsswp2004 <31414054+jsswp2004@users.noreply.github.com>
2022-01-26 23:38:57 -0500

    created dipatch for cart-dropdown

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -7,13 +7,14 @@ import { useNavigate } from "react-router-dom";
 import CustomButton from "../custom-button/custom-button.component";
 import CartItem from "../cart-item/cart-item.component";
 import { selectCartItems } from "../../redux/cart/cart.selectors";
-
+import { toggleCartHidden } from "../../redux/cart/cart.actions";
 import "./cart-dropdown.styles.scss";
 
-const CartDropdown = ({ cartItems }) => {
+const CartDropdown = ({ cartItems, dispatch }) => {
   const navigate = useNavigate();
   const handleClick = () => {
     navigate("/checkout");
+    dispatch(toggleCartHidden());
   };
   return (
     <div className="cart-dropdown">
