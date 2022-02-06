2e19fc66181d1b32b3e3141515a55ba83e7f8cb3
jsswp2004 <31414054+jsswp2004@users.noreply.github.com>
2022-02-05 22:54:31 -0500

    150

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import CategoryPage from "../category/category.component";
 //import { connect } from 'react-redux';
 //import { createStructuredSelector } from "reselect";
 
@@ -7,14 +9,30 @@ import CollectionsOverview from "../../components/collections-overview/collectio
 
 //import { selectCollections } from "../../redux/shop/shop.selectors.js";
 
-const ShopPage = ({ collections }) => (
-  <div className="shop-page">
-    <CollectionsOverview />
-  </div>
-);
-
+//const ShopPage = ({ match }) => {
+//  console.log(match);
+//  return (
+//    <div className="shop-page">
+//      <Routes>
+//        <Route path={`${match.path}`} element={<CollectionsOverview/>} />
+//      </Routes>
+//    </div>
+//  );
+//};
+const ShopPage = () => {
+  //console.log();
+  return (
+    <div className="shop-page">
+      <Routes>
+        <Route path="/" element={<CollectionsOverview />} />
+        <Route path=":categoryId" element={<CategoryPage />} />
+      </Routes>
+    </div>
+  );
+};
 //const mapStateToProps = createStructuredSelector({
 //  collections: selectCollections,
 //});
 
 export default ShopPage;
+//<Route path=":categoryId" element={<CategoryPage />} />
